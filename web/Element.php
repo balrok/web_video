e9cae8cb2f094197d412a54fd59f1bc4988db076
Carl Mai <carl.mai@tu-dresden.de>
2017-01-14 20:18:12 +0100

    php-cs-fixer

--- web/Element.php
+++ web/Element.php
@@ -3,33 +3,38 @@ namespace balrok\web_video\web;
 
 class Element
 {
-	public $item;  // @var string                                     // path to the element
-	public $name;  // @var string
-	public $sprite;
-	public $type = 'pic';
+    public $item;  // @var string                                     // path to the element
+    public $name;  // @var string
+    public $sprite;
+    public $type = 'pic';
 
-	public function __construct(string $file, string $dir, string $type)
-	{
-		$this->name = $file;
+    public function __construct(string $file, string $dir, string $type)
+    {
+        $this->name = $file;
         $this->dir = $dir;
         $this->type = $type;
 
-		$this->item = $dir.'/'.$file;
-		$this->thumb= $this->item;
-		$this->sprite = '';
-		if (file_exists($dir.'/'.$this->name.'/sprite.jpg'))
-			$this->sprite = $dir.'/'.$this->name.'/sprite.jpg';
-	}
+        $this->item = $dir.'/'.$file;
+        $this->thumb= $this->item;
+        $this->sprite = '';
+        if (file_exists($dir.'/'.$this->name.'/sprite.jpg')) {
+            $this->sprite = $dir.'/'.$this->name.'/sprite.jpg';
+        }
+    }
 
-	public function getImage($type="") {
-		if ($this->type == 'pic') {
-            if ($type == "")
+    public function getImage($type="")
+    {
+        if ($this->type == 'pic') {
+            if ($type == "") {
                 $type = "2048";
-            if (is_dir($this->item) && file_exists($this->item . '/'. $type . ".jpg"))
+            }
+            if (is_dir($this->item) && file_exists($this->item . '/'. $type . ".jpg")) {
                 return $this->item . '/' . $type . ".jpg";
-			return $this->item;
-        } else if ($this->type == 'video')
-			return $this->item.'/img_3.jpg';
-		return false;
-	}
+            }
+            return $this->item;
+        } elseif ($this->type == 'video') {
+            return $this->item.'/img_3.jpg';
+        }
+        return false;
+    }
 }
--- web/Gallery.php
+++ web/Gallery.php
@@ -3,83 +3,91 @@ namespace balrok\web_video\web;
 
 class Gallery
 {
-	public $folder;
-	public $title;
-	public $descr = '';
-	public $dir;
-	public $elements = [];
+    public $folder;
+    public $title;
+    public $descr = '';
+    public $dir;
+    public $elements = [];
 
-	public function __construct(string $folder, string $dir) {
-		$dir .= '/'.$folder;
-		$this->folder = $folder;
-		if (!file_exists($dir) || !is_dir($dir) || strpos($dir, '..') == -1) {
-			echo 'strange directory '.$dir;
-			return NULL;
-		}
+    public function __construct(string $folder, string $dir)
+    {
+        $dir .= '/'.$folder;
+        $this->folder = $folder;
+        if (!file_exists($dir) || !is_dir($dir) || strpos($dir, '..') == -1) {
+            echo 'strange directory '.$dir;
+            return null;
+        }
 
-		$this->dir = $dir;
+        $this->dir = $dir;
 
-		if (!$this->readInfo()) {
-			echo "Missing info.txt file in: ".$this->dir;
-			return null;
-		}
-		$this->readFiles();
-	}
+        if (!$this->readInfo()) {
+            echo "Missing info.txt file in: ".$this->dir;
+            return null;
+        }
+        $this->readFiles();
+    }
 
-	public function readInfo() {
-		if (file_exists($this->dir.'/info.txt')) {
-			$info = @fopen($this->dir.'/info.txt', "r");
-			if ($info)
-			{
-				$content = '';
-				while (!feof($info))
-					$content .= fgets($info, 4096);
-				$rows = explode("\n", $content);
-			}
-			$this->title = $rows[0];
+    public function readInfo()
+    {
+        if (file_exists($this->dir.'/info.txt')) {
+            $info = @fopen($this->dir.'/info.txt', "r");
+            if ($info) {
+                $content = '';
+                while (!feof($info)) {
+                    $content .= fgets($info, 4096);
+                }
+                $rows = explode("\n", $content);
+            }
+            $this->title = $rows[0];
             unset($rows[0]);
-			$this->descr = implode("<br/>\n", $rows);
-		} else {
-			return false;
-		}
-		return true;
-	}
+            $this->descr = implode("<br/>\n", $rows);
+        } else {
+            return false;
+        }
+        return true;
+    }
 
-	public function readFiles() {
-		$dir_handle = opendir($this->dir);
-		$files = array();
-		while ($file = readdir($dir_handle)) {
-			if ($this->is_pic($file) || $this->is_video_folder($file)) {
-				$files[] = $file;
-			}
-		}
-		sort($files);
-		for ($i = 0; $i < count($files); ++$i)
-		{
+    public function readFiles()
+    {
+        $dir_handle = opendir($this->dir);
+        $files = array();
+        while ($file = readdir($dir_handle)) {
+            if ($this->is_pic($file) || $this->is_video_folder($file)) {
+                $files[] = $file;
+            }
+        }
+        sort($files);
+        for ($i = 0; $i < count($files); ++$i) {
             $type = $this->is_pic($files[$i])?'pic':($this->is_video_folder($files[$i])?'video':'');
-			$this->elements[] = new Element($files[$i], $this->dir, $type);
-		}
-	}
+            $this->elements[] = new Element($files[$i], $this->dir, $type);
+        }
+    }
 
-	protected function isEnabled($i, $file) {
-		return true;
-	}
+    protected function isEnabled($i, $file)
+    {
+        return true;
+    }
 
-	protected function getFileDescription($i, $file) {
-		return "";
-	}
+    protected function getFileDescription($i, $file)
+    {
+        return "";
+    }
 
-	public function is_pic($file) {
-        if (file_exists($this->dir.'/'.$file.'/256.jpg'))
+    public function is_pic($file)
+    {
+        if (file_exists($this->dir.'/'.$file.'/256.jpg')) {
             return true;
-		$ending = strtolower(substr($file, -3));
-		return in_array($ending, array('jpg', 'png', 'gif'));
-	}
-	public function is_video_folder($file) {
-		return file_exists($this->dir.'/'.$file.'/img_3.jpg');
-	}
+        }
+        $ending = strtolower(substr($file, -3));
+        return in_array($ending, array('jpg', 'png', 'gif'));
+    }
+    public function is_video_folder($file)
+    {
+        return file_exists($this->dir.'/'.$file.'/img_3.jpg');
+    }
 
-    public function getImage($type="256x") {
+    public function getImage($type="256x")
+    {
         foreach ($this->elements as $el) {
             if ($el->type == 'pic') {
                 return $el->getImage($type);
@@ -93,7 +101,8 @@ class Gallery
         return false;
     }
 
-    public function getSprite() {
+    public function getSprite()
+    {
         foreach ($this->elements as $el) {
             if ($el->type == 'video' && $el->sprite) {
                 return $el->sprite;
@@ -101,5 +110,4 @@ class Gallery
         }
         return false;
     }
-
 }
